97e9f723f7b34000042ad3ebd505a834f7bb47a9
harshani <ruwanthikapemarathna@gmail.com>
2024-11-13 13:29:17 +0530

    initial commit

---
+++ BackEnd/smartCart/src/test/java/com/harshani/smartCart/SmartCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harshani.smartCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
